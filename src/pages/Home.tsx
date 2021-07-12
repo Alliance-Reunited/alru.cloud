e07aaed364e08ff411590866e4f90b0f7fb22dbb
Damien Vesper <ldamienvesper@gmail.com>
2021-07-11 22:28:24 -0500

    pushed latest zzz

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,10 @@ import '../../public/assets/scss/pages/home.scss';
 class Home extends React.Component {
     render = () => {
         return (
-            <main className="text-center container mt-5"></main>
+            <main className="text-center mt-5">
+                <h1>Welcome to the Alru Network!</h1>
+                <iframe src="https://canary.discord.com/widget?id=757079346719621150&theme=dark" width="350" height="500" allowTransparency={true} frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+            </main>
         );
     }
 }
