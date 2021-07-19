ba7e93bf3536b4e2e42f401b05f092abc1c91ea9
Damien Vesper <ldamienvesper@gmail.com>
2021-07-18 22:10:41 -0500

    home: wrap it

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,9 +12,11 @@ import '../../public/assets/scss/pages/home.scss';
 class Home extends React.Component {
     render = () => {
         return (
-            <main className="text-center mt-5">
-                <h1>Welcome to the Alru Network!</h1>
-                <iframe src="https://canary.discord.com/widget?id=757079346719621150&theme=dark" width="350" height="500" allowTransparency={true} frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+            <main className="text-center">
+                <h1 className="mt-5 mb-3">Welcome to the Alru Network!</h1>
+                <div className="sidebar-wrapper">
+                    <iframe src="https://canary.discord.com/widget?id=757079346719621150&theme=dark" width="350" height="500" allowTransparency={true} frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                </div>
             </main>
         );
     }
