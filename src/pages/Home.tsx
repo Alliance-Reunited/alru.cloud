e20da193487a15c159f441b0c5b059a13c600c02
Damien Vesper <ldamienvesper@gmail.com>
2021-07-18 23:59:18 -0500

    add animations

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,7 +14,7 @@ class Home extends React.Component {
         return (
             <main className="text-center container">
                 <h1 className="my-5">Welcome to the Alru Network!</h1>
-                <p>Reuniting friends, family, and forgotten organizations, we are a community bringing together these scattered groups as one. We&apos;ve also been hosting servers for various different games since November 15, 2020, for the community, and more. Join our Discord to be part of our amazing community today!</p>
+                <p className="desc">Reuniting friends, family, and forgotten organizations, we are a community bringing together these scattered groups as one. We&apos;ve also been hosting servers for various different games since November 15, 2020, for the community, and more. Join our Discord to be part of our amazing community today!</p>
             </main>
         );
     }
