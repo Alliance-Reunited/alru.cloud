49c6f22dbdd5be19a10571ecdca8c64f460d36c7
Lion <phixmeplease@gmail.com>
2021-07-20 03:34:35 -0400

    more 404 improvements

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -12,6 +12,7 @@ class Error404 extends React.Component {
             <main className="text-center">
                 <h1 className="mt-5 error-header">404</h1>
                 <h3 className="error-subscript">Sorry, the link you clicked was an imposter!</h3>
+                <h4 className="error-subsubscript">Join our Garry's Mod Among Us server so you can be an imposter too!</h4>
             </main>
         );
     }
