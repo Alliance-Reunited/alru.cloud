e9c399addd6050f855a41f0d2b38102a1fc2d6aa
Damien Vesper <ldamienvesper@gmail.com>
2021-07-19 22:04:30 -0500

    home: fix page title animation from taking the rest of the damn page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ class Home extends React.Component {
     render = () => {
         return (
             <main className="text-center container">
-                <h1 className="my-5">Welcome to the Alru Network!</h1>
+                <h1 className="page-title my-5">Welcome to the Alru Network!</h1>
                 <p className="desc">Reuniting friends, family, and forgotten organizations, we are a community bringing together these scattered groups as one. We&apos;ve also been hosting servers for various different games since November 15, 2020, for the community, and more. Join our Discord to be part of our amazing community today!</p>
                 <div className="container mt-5">
                     <a href="/servers#gmod" className="gmod-btn btn btn-lg mx-1">Garry's Mod</a>
