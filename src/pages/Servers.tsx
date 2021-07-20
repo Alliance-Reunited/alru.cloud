d28221130de49d663606cb39688fc4c1192ce191
Lion <phixmeplease@gmail.com>
2021-07-19 23:15:19 -0400

    servers

--- src/pages/Servers.tsx
+++ src/pages/Servers.tsx
@@ -10,7 +10,36 @@ import '../../public/assets/scss/pages/servers.scss';
 class Servers extends React.Component {
     render = () => {
         return (
-            <main className="text-center container"></main>
+            <main className="text-center container mt-5">
+                <h1>Servers</h1>
+                <h2>We have coolness</h2>
+
+                <br />
+                <br />
+                <h3 className="gmod-title">Garry's Mod</h3>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Server</th>
+                            <th scope="col">IP</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Family Guy DarkRP</td>
+                            <td><a href="steam://connect/207.225.26.83:65470">207.225.26.83:65470</a></td>
+                        </tr>
+                        <tr>
+                            <td>Among Us</td>
+                            <td><a href="steam://207.225.26.83:65443">207.225.26.83:65443</a></td>
+                        </tr>
+                        <tr>
+                            <td><span className="bad">[COMING SOON]</span> South Park DarkRP</td>
+                            <td><a href="steam://connect/207.225.26.83:65448">207.225.26.83:65448</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </main>
         );
     }
 }
