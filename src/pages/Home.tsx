54658f89c3d419950960ea3c651b4272634da53a
Damien Vesper <ldamienvesper@gmail.com>
2021-07-19 22:06:16 -0500

    fix this one too

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,7 +15,7 @@ class Home extends React.Component {
             <main className="text-center container">
                 <h1 className="page-title my-5">Welcome to the Alru Network!</h1>
                 <p className="desc">Reuniting friends, family, and forgotten organizations, we are a community bringing together these scattered groups as one. We&apos;ve also been hosting servers for various different games since November 15, 2020, for the community, and more. Join our Discord to be part of our amazing community today!</p>
-                <div className="container mt-5">
+                <div className="game-btn-wrapper container mt-5">
                     <a href="/servers#gmod" className="gmod-btn btn btn-lg mx-1">Garry's Mod</a>
                     <a href="/servers#minecraft" className="minecraft-btn btn btn-lg mx-1">Minecraft</a>
                     <a href="/servers#fivem" className="fivem-btn btn btn-lg mx-1">FiveM</a>
