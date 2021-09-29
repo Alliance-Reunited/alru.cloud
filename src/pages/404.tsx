3339f69884a46abb077f976f4d195a9e1bc47176
Damien Vesper <ldamienvesper@gmail.com>
2021-09-28 20:24:17 -0500

    enhance: fix 404 animation + remove unscrupulous content

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,8 +6,7 @@ class Error404 extends React.Component {
     render = (): React.ReactNode => (
         <main className="text-center">
             <h1 className="mt-5 error-header">404</h1>
-            <h3 className="error-subscript">Sorry, the link you clicked was an imposter!</h3>
-            <h4 className="error-subsubscript">Join our Garry&apos;s Mod Among Us server so you can be an imposter too!</h4>
+            <h3 className="error-subscript">The page you were looking for could not be found!</h3>
         </main>
     );
 }
