8d18e2069270c98757f7ca6f4b46f8174b83dafa
Benton J <phixmeplease@gmail.com>
2022-01-03 17:10:41 -0500

    server panel

--- alru.cloud/pages/servers/index.js
+++ alru.cloud/pages/servers/index.js
@@ -6,8 +6,15 @@ import Footer from "../../components/Footer";
 import styles from "../../styles/pages/servers.module.scss";
 
 function ServerPanel(props) {
-    return(<div className={}>
-        
+    return(<div className={styles.serverPanel}>
+        <div className={styles.previewImage}>
+            <img src={props.image}></img>    
+        </div> {/* Preview image made with background on a div */}
+        <div className={styles.info}>
+            <p className={styles.servername}>{props.title}</p>
+            <p className={styles.serverconn}>{props.ip}</p>
+            <a href={`/servers/${props.url}`} className={styles.serverrules}>Rules</a>
+        </div>
     </div>)
 }
 
@@ -20,7 +27,11 @@ function ServersIndex() {
             <p className={styles.subtitle}>Check out our amazing GameServers</p>
         </div>
 
-        <ServerPanel title="PrisonRP" ip="localhost" url="prisonrp"/>
+        <div className={styles.serversPanel}>
+            <ServerPanel title="PrisonRP" ip="localhost" url="prisonrp" image="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png"/>
+            <ServerPanel title="DarkRP" ip="localhost" url="darkrp"/>
+            <ServerPanel title="AmericanRP" ip="localhost" url="americanrp"/>
+        </div>
 
         <Footer />
     </div>)
